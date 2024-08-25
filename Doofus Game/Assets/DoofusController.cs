2ab40b4e6f4eedc4242773bcc94bdb571a9e83f3
Barath_M <barathbommannan@gmail.com>
2024-08-25 22:40:51 +0530

    Created DoofusController.cs for Doofus's movements.

---
+++ Doofus Game/Assets/DoofusController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoofusController : MonoBehaviour
+{
+    public float speed=.3f;
+    void Update()
+    {
+        float xDirection=Input.GetAxis("Horizontal");
+        float zDirection=Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection,0.0f,zDirection);
+        transform.position+=moveDirection*speed;
+
+    }
+}
