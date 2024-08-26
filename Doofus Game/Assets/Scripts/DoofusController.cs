ec86ed88fac0dc2f7d7e4a5113cf06b3619dbc1b
Barath_M <barathbommannan@gmail.com>
2024-08-26 23:58:29 +0530

    Modified the script to display Pulpit destroy timer.

--- Doofus Game/Assets/Scripts/DoofusController.cs
+++ Doofus Game/Assets/Scripts/DoofusController.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class DoofusController : MonoBehaviour
 {
+    private const string WALK_ANIM_PARAMETER = "Walk";
     public float speed = 10f;
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.speed = 0;
+    }
 
     void Update()
     {
@@ -12,6 +21,13 @@ public class DoofusController : MonoBehaviour
         float zDirection=Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(xDirection,0.0f,zDirection);
+        
         transform.position += moveDirection * speed * Time.deltaTime;
+
+        bool isMoving = xDirection != 0 || zDirection != 0;
+
+        if (isMoving) transform.rotation = Quaternion.LookRotation(moveDirection);
+
+        animator.speed = isMoving ? 1f : 0f;
     }
 }
--- Doofus Game/Assets/Scripts/PulpitManager.cs
+++ Doofus Game/Assets/Scripts/PulpitManager.cs
@@ -1,109 +1,97 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PulpitManager : MonoBehaviour
 {
     public GameObject pulpitPrefab;
-    public Transform startPoint;
 
     public float minPulpitDestroyTime = 4f;
     public float maxPulpitDestroyTime = 5f;
     public float pulpitSpawnTime = 2.5f;
-    public float initialDelay = 1f;
-    public float minDistance = 9f;
 
-    private Vector3 lastPosition;
+    public float minDistance = 9f;
+    
     private GameObject currentPulpit;
-    private GameObject previousPulpit;
-    private bool isSpawning = true;
+    private GameObject nextPulpit;
+    
+    private bool isGameOver = false;
 
     void Start()
     {
-        lastPosition = Vector3.zero;
+        float pulpitLifeTime = Random.Range(minPulpitDestroyTime, maxPulpitDestroyTime);
 
-        currentPulpit = Instantiate(pulpitPrefab, lastPosition, Quaternion.identity);
-        previousPulpit = currentPulpit;
+        currentPulpit = Instantiate(pulpitPrefab, Vector3.zero, Quaternion.identity);
+        StartCoroutine(UpdateCountdown(currentPulpit, pulpitLifeTime));
+        StartCoroutine(HideAndDestroyPulpit(currentPulpit, pulpitLifeTime));
 
-        StartCoroutine(SpawnPulpitWithInitialDelay());
+        StartCoroutine(SpawnNextPulpits());
     }
 
-    IEnumerator SpawnPulpitWithInitialDelay()
+    IEnumerator SpawnNextPulpits()
     {
-        yield return new WaitForSeconds(initialDelay);
-
-        StartCoroutine(SpawnPulpit());
-    }
-
-    IEnumerator SpawnPulpit()
-    {
-        while (isSpawning)
+        while (!isGameOver)
         {
-            float waitTime = Random.Range(minPulpitDestroyTime, maxPulpitDestroyTime);
-            float destroyTime = Mathf.Max(waitTime - pulpitSpawnTime, 0);
+            yield return new WaitForSeconds(pulpitSpawnTime);
+                
+            float nextPulpitLifetime = Random.Range(minPulpitDestroyTime, maxPulpitDestroyTime);
+            Vector3 nextPulpitSpawnPos = GetRandomNextPulpitPos();
 
-            Vector3 spawnPosition = GetRandomDirection();
-            currentPulpit = Instantiate(pulpitPrefab, spawnPosition, Quaternion.identity);
-
-            // Get the countdown text component from the current pulpit
-            Text countdownText = currentPulpit.GetComponentInChildren<Text>();
-            StartCoroutine(UpdateCountdown(countdownText, destroyTime));
-
-            if (previousPulpit != null)
-            {
-                StartCoroutine(HideAndDestroyPulpit(previousPulpit, destroyTime));
-            }
-
-            previousPulpit = currentPulpit;
-
-            lastPosition = spawnPosition;
-
-            yield return new WaitForSeconds(waitTime);
+            nextPulpit = Instantiate(pulpitPrefab, nextPulpitSpawnPos, Quaternion.identity);
+            StartCoroutine(UpdateCountdown(nextPulpit, nextPulpitLifetime));
+            StartCoroutine(HideAndDestroyPulpit(nextPulpit, nextPulpitLifetime));
         }
     }
 
-    IEnumerator HideAndDestroyPulpit(GameObject pulpit, float delay)
+    IEnumerator HideAndDestroyPulpit(GameObject pulpit, float lifeTime)
     {
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSeconds(lifeTime);
 
-        MeshRenderer renderer = pulpit.GetComponent<MeshRenderer>();
-        if (renderer != null)
-        {
-            renderer.enabled = false;
-        }
-
-        Destroy(pulpit, 0);
+        currentPulpit = nextPulpit;
+        
+        Destroy(pulpit);
     }
 
-    IEnumerator UpdateCountdown(Text countdownText, float countdownTime)
+    IEnumerator UpdateCountdown(GameObject pulpit, float lifeTime)
     {
-        while (countdownTime > 0)
+        TextMeshPro countdownText = pulpit.GetComponentInChildren<TextMeshPro>();
+
+        while (lifeTime > 0)
         {
-            if (countdownText != null)
-            {
-                countdownText.text = "Destroy in: " + countdownTime.ToString("F1") + "s";
-            }
-            countdownTime -= Time.deltaTime;
+            countdownText.text = lifeTime.ToString("F1") + "s";
+
+            lifeTime -= Time.deltaTime;
             yield return null;
         }
 
-        if (countdownText != null)
-        {
-            countdownText.text = "";
-        }
+        countdownText.text = "0s";
     }
 
-    Vector3 GetRandomDirection()
+    Vector3 GetRandomNextPulpitPos()
     {
-        Vector3 direction;
-        if (Random.Range(0, 2) == 0)
+        Vector3 currentPulpitPos = currentPulpit.transform.position;
+        Vector3 nextPulpitPos = Vector3.zero;
+
+        int randomDirectionIndex = Random.Range(0, 4);
+
+        if (randomDirectionIndex == 0)
         {
-            direction = new Vector3(lastPosition.x + minDistance, lastPosition.y, lastPosition.z);
+            nextPulpitPos = new Vector3(currentPulpitPos.x + minDistance, currentPulpitPos.y, currentPulpitPos.z);
         }
-        else
+        else if (randomDirectionIndex == 1)
         {
-            direction = new Vector3(lastPosition.x, lastPosition.y, lastPosition.z + minDistance);
+            nextPulpitPos = new Vector3(currentPulpitPos.x, currentPulpitPos.y, currentPulpitPos.z + minDistance);
         }
-        return direction;
+        else if (randomDirectionIndex == 2)
+        {
+            nextPulpitPos = new Vector3(currentPulpitPos.x - minDistance, currentPulpitPos.y, currentPulpitPos.z);
+        }
+        else if (randomDirectionIndex == 3)
+        {
+            nextPulpitPos = new Vector3(currentPulpitPos.x, currentPulpitPos.y, currentPulpitPos.z - minDistance);
+        }
+
+        return nextPulpitPos;
     }
 }
